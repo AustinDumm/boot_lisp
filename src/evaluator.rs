9238fd0867a5af13fb390f23f139b5cffae7d21d
bowman3002 <dummmagic@gmail.com>
2021-08-10 21:01:42 +0000

    Update map extension to including binding exprs

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -2,11 +2,8 @@
 use std::collections::HashMap;
 
 use std::sync::{
-    Mutex,
     RwLock,
     Arc,
-    RwLockReadGuard,
-    RwLockWriteGuard,
 };
 
 use crate::parser::{
@@ -49,8 +46,63 @@ impl Env {
         }
     }
 
+    // TODO: Setup binding_list and bindings to populate new hashmap
     pub fn extend(self_ptr: Arc<Self>, binding_list: Expr, bindings: Expr) -> Option<Arc<Self>> {
-        Some(Arc::new(Env { dict: HashMap::new(), next_env: Some(self_ptr) }))
+        fn collect_binding_pairs(mut binding_list: Expr, mut bindings: Expr) -> Vec<(String, Expr)> {
+            if let ExprData::Identifier(name) = binding_list.expr_data {
+                vec![(name, bindings)]
+            } else {
+                match (binding_list.expr_data, bindings.expr_data) {
+                    (ExprData::List(raw_bindings), ExprData::List(raw_values)) => {
+                        if raw_bindings.len() == raw_values.len() {
+                            raw_bindings
+                                .iter()
+                                .map(|expr| match &expr.expr_data {
+                                                ExprData::Identifier(name) => name.clone(),
+                                                _ => panic!("Attempt to bind value to non-identifier")
+                                })
+                                .zip(raw_values.into_iter()).collect()
+                        } else {
+                            panic!("Attempt to bind with unmatching list length");
+                        }
+                    },
+                    (ExprData::DottedList(raw_bindings, final_binding), ExprData::List(raw_values)) => {
+                        let mut pairs: Vec<(String, Expr)> = vec![];
+                        let bindings_iter = raw_bindings.into_iter();
+                        let values_iter = raw_values.into_iter();
+                        let mut zipped_iter = bindings_iter.zip(values_iter);
+                        while let Some(pair) = zipped_iter.next() {
+                            let (binding, value) = pair;
+                            if let ExprData::Identifier(name) = binding.expr_data {
+                                pairs.push((name, value));
+                            } else {
+                                panic!("Invalid binding identifier")
+                            }
+                        }
+
+                        let (bindings_iter, values_iter): (Vec<_>, Vec<_>) = zipped_iter.unzip();
+
+                        if bindings_iter.len() != 0 {
+                            panic!("Unmatched binding arity. Too few values to bind");
+                        }
+
+                        if let ExprData::Identifier(name) = final_binding.expr_data {
+                            pairs.push((name, Expr::form_list(values_iter)))
+                        } else {
+                            panic!("Final expr in dotted binding list is not an identifier")
+                        }
+
+                        pairs
+                    },
+                    (_, _) => panic!("Invalid binding expression attempted to bind in environment")
+                }
+            }
+        }
+
+        let binding_pairs = collect_binding_pairs(binding_list, bindings)
+            .into_iter()
+            .map(|pair| (pair.0, Arc::new(RwLock::new(pair.1))));
+        Some(Arc::new(Env { dict: binding_pairs.collect(), next_env: Some(self_ptr) }))
     }
 }
 
